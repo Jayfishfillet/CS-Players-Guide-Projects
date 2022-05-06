e2d18b14f70066567d89800befb85ca41e818e4a
STEEL\Jay <jay.sanchez287@gmail.com>
2022-05-06 16:56:19 -0400

    Refactored Simula's Soup

--- Part 2 - Object Oriented Programming/Simula's Soup/Program.cs
+++ Part 2 - Object Oriented Programming/Simula's Soup/Program.cs
@@ -1,93 +1,88 @@
 ﻿Console.Title = "Simula's Soup";
 
 (Food food, Ingredient ingredient, Seasoning seasoning) cookedDish = MealCombination();
+Console.WriteLine($"You've chosen to make a {cookedDish.seasoning} {cookedDish.ingredient} {cookedDish.food}, tasty!");
 
 (Food, Ingredient, Seasoning) MealCombination()
 {
-    Food foodChoice = 0;
-    Ingredient ingredientChoice = 0;
-    Seasoning seasoningChoice = 0;
-    bool retryChoice;
-
-    do
-    {
-        Console.WriteLine("What type of meal would you like? We have Soup, Stew, and Gumbo.");
-        retryChoice = false;
-        switch (Console.ReadLine().ToLower())
-        {
-            case "soup":
-                foodChoice = Food.soup;
-                break;
-            case "stew":
-                foodChoice = Food.stew;
-                break;
-            case "gumbo":
-                foodChoice = Food.gumbo;
-                break;
-            default:
-                Console.WriteLine("We don't have that here.\n");
-                retryChoice = true;
-                break;
-        }
-    }
-    while (retryChoice == true);
-
-    do
-    {
-        Console.WriteLine("What ingredient would you like? Mushrooms, Chicken, Carrots, and Potaroes.");
-        retryChoice = false;
-        switch (Console.ReadLine().ToLower())
-        {
-            case "mushrooms":
-                ingredientChoice = Ingredient.mushrooms;
-                break;
-            case "chicken":
-                ingredientChoice = Ingredient.chicken;
-                break;
-            case "carrots":
-                ingredientChoice = Ingredient.carrots;
-                break;
-            case "potatoes":
-                ingredientChoice = Ingredient.potatoes;
-                break;
-            default:
-                Console.WriteLine("We don't have that here.\n");
-                retryChoice = true;
-                break;
-        }
-    } 
-    while (retryChoice == true);
-
-    do
-    {
-        Console.WriteLine("What seasoning would you prefer? Spicy, Salty, or Sweet?");
-        retryChoice = false;
-        switch (Console.ReadLine().ToLower())
-        {
-            case "spicy":
-                seasoningChoice = Seasoning.spicy;
-                break;
-            case "salty":
-                seasoningChoice = Seasoning.salty;
-                break;
-            case "sweet":
-                seasoningChoice = Seasoning.sweet;
-                break;
-            default:
-                Console.WriteLine("We don't have that here.\n");
-                retryChoice = true;
-                break;
-        }
-    }
-    while (retryChoice == true);
+    Food foodChoice = GetFoodChoice();
+    Ingredient ingredientChoice = GetIngredientChoice();
+    Seasoning seasoningChoice = GetSeasoningChoice();
 
     return (foodChoice, ingredientChoice, seasoningChoice);
 
 
 }
 
-
-Console.WriteLine($"You've chosen to make a {cookedDish.seasoning} {cookedDish.ingredient} {cookedDish.food}, tasty!");
+Food GetFoodChoice()
+{
+    while(true)
+    {
+        Console.WriteLine("What type of meal would you like? We have Soup, Stew, and Gumbo.");
+        switch (Console.ReadLine().ToLower())
+        {
+            case "soup":
+                return Food.soup;
+                break;
+            case "stew":
+                return Food.stew;
+                break;
+            case "gumbo":
+                return Food.gumbo;
+                break;
+            default:
+                Console.WriteLine("We don't have that here.\n");
+                break;
+        }
+    }
+}
+Ingredient GetIngredientChoice()
+{
+    while (true)
+    {
+        Console.WriteLine("What ingredient would you like? Mushrooms, Chicken, Carrots, and Potatoes.");
+        switch (Console.ReadLine().ToLower())
+        {
+            case "mushrooms":
+                return Ingredient.mushrooms;
+                break;
+            case "chicken":
+                return Ingredient.chicken;
+                break;
+            case "carrots":
+                return Ingredient.carrots;
+                break;
+            case "potatoes":
+                return Ingredient.potatoes;
+                break;
+            default:
+                Console.WriteLine("We don't have that here.\n");
+                break;
+        }
+    }
+}
+Seasoning GetSeasoningChoice()
+{
+    while(true)
+    {
+        Console.WriteLine("What seasoning would you prefer? Spicy, Salty, or Sweet?");
+        switch (Console.ReadLine().ToLower())
+        {
+            case "spicy":
+                return Seasoning.spicy;
+                break;
+            case "salty":
+                return Seasoning.salty;
+                break;
+            case "sweet":
+                return Seasoning.sweet;
+                break;
+            default:
+                Console.WriteLine("We don't have that here.\n");
+                break;
+        }
+    }
+}
 
 enum Food {soup, stew, gumbo};
 enum Ingredient {mushrooms, chicken, carrots, potatoes}

d0b7815566b8e7c8b07c9decda313dda5dffc0d7
STEEL\Jay <jay.sanchez287@gmail.com>
2022-05-05 23:25:39 -0400

    Fixed Structure

---
+++ Part 2 - Object Oriented Programming/Simula's Test/Program.cs
@@ -0,0 +1,81 @@
+﻿Chest chestStatus = Chest.locked;
+
+
+while (true)
+{
+    Ask();
+    switch (Console.ReadLine().ToLower())
+    {
+        case "unlock":
+            if (chestStatus == Chest.locked)
+            {
+                chestStatus = Chest.closed;
+            }
+            else if (chestStatus == Chest.open)
+            {
+                Console.WriteLine($"Chest can't be opened");
+            }
+            else if (chestStatus == Chest.closed)
+            {
+                Console.WriteLine("Chest is alreay unlocked.");
+            }
+            break;
+
+        case "open":
+            if (chestStatus == Chest.locked)
+            {
+                Console.WriteLine($"Chest can not be opened.");
+            }
+            else if (chestStatus == Chest.open)
+            {
+                Console.WriteLine($"Chest is already {chestStatus}.");
+            }
+            else if (chestStatus == Chest.closed)
+            {
+                chestStatus = Chest.open;
+            }
+            break;
+
+        case "lock":
+            if (chestStatus == Chest.locked)
+            {
+                Console.WriteLine($"Chest is already {chestStatus}.");
+            }
+            else if (chestStatus == Chest.open)
+            {
+                Console.WriteLine($"Chest must be closed first.");
+            }
+            else if (chestStatus == Chest.closed)
+            {
+                chestStatus = Chest.locked;
+            }
+            break;
+
+        case "close":
+            if (chestStatus == Chest.locked)
+            {
+                Console.WriteLine($"Chest is already closed.");
+            }
+            else if (chestStatus == Chest.open)
+            {
+                chestStatus = Chest.closed;
+            }
+            else if (chestStatus == Chest.closed)
+            {
+                Console.WriteLine($"Chest is already {chestStatus}.");
+            }
+            break;
+        default:
+            Console.WriteLine("That doesnt seem to work.");
+            break;
+
+    }
+}
+
+
+void Ask()
+{
+    Console.Write($"Chest is {chestStatus}. What will you do? ");
+}
+
+enum Chest  { locked, closed, open  };

c9af6535c5c80d714ddd2ff339520a8d8dd44876
STEEL\Jay <jay.sanchez287@gmail.com>
2022-05-11 13:33:00 -0400

    Completed Boss Battle - The Point

---
+++ Part 2 - Object Oriented Programming/Boss Battle - The Point/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Title = "The point";
+
+Point originPoint = new Point();
+Point firstPoint = new Point(2, 3);
+Point secondPoint = new Point(-4, 0);
+
+Console.WriteLine($"{firstPoint.xpos}, {firstPoint.ypos}");
+Console.WriteLine($"{secondPoint.xpos},{secondPoint.ypos}");
+
+
+
+class Point
+{ 
+    public int xpos { get;}
+    public int ypos { get;}
+
+    public Point()
+    {
+        this.xpos = 0;
+        this.ypos = 0;
+    }
+
+    public Point(int xpos, int ypos)
+    { 
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+}

eae2f130ba4034e2ac03057899a11bf8c48aaa33
STEEL\Jay <jay.sanchez287@gmail.com>
2022-05-12 12:41:00 -0400

    Completed 'The Color' change name of 'The Point'

---
+++ Part 2 - Object Oriented Programming/Boss Battle - The Card/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Title = "The Card";
+
+CardColor[] color = new CardColor[4] { CardColor.Red, CardColor.Blue, CardColor.Green, CardColor.Yellow };
+CardRank[] rank = new CardRank[14] { CardRank.One, CardRank.Two, CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven, CardRank.Eight, CardRank.Nine, CardRank.Ten, CardRank.DollarSign, CardRank.Percent, CardRank.Caret, CardRank.Ampersand };
+
+foreach (CardColor cardColor in color)
+{
+    foreach (CardRank cardRank in rank)
+    {
+        Card newCard = new Card(cardColor, cardRank);
+        Console.WriteLine($"Is Number:{newCard.IsNumber()}\nThe {newCard.CardColor} {newCard.CardRank}\n" );
+    }
+}
+class Card
+{
+    public CardColor CardColor { get; }
+    public CardRank CardRank { get; }
+
+    public Card(CardColor cardColor, CardRank cardRank)
+    {
+        CardColor = cardColor;
+        CardRank = cardRank;
+    }
+
+    public bool IsNumber() //if its a symbol, it will return false
+    {
+        bool isNumber = CardRank switch
+        {
+            CardRank.DollarSign => false,
+            CardRank.Percent => false,
+            CardRank.Caret => false,
+            CardRank.Ampersand => false,
+            _ => true
+        };
+        return isNumber;
+    }
+}
+
+enum CardColor {Red, Blue, Green, Yellow}
+enum CardRank {One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand}

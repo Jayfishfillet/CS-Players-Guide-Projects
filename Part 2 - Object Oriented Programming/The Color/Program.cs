04caaace4e91a6f4c2c525d13598cf0a876a4234
STEEL\Jay <jay.sanchez287@gmail.com>
2022-05-12 04:56:08 -0400

    Added Console.WriteLine for 'The Color'

--- Part 2 - Object Oriented Programming/The Color/Program.cs
+++ Part 2 - Object Oriented Programming/The Color/Program.cs
@@ -3,21 +3,23 @@
 Color firstColor = Color.Purple;
 Color secondColor = new Color(12, 21, 36);
 
+Console.WriteLine($"Purple = ({firstColor.R}, {firstColor.G}, {firstColor.B})");
+Console.WriteLine($"Custom Color = ({secondColor.R}, {secondColor.G}, {secondColor.B})");
+
 class Color
 {
     public int R { get; }
     public int G { get; }
     public int B { get; }
 
-    public static Color White = new Color(255, 255, 255);
-    public static Color Black = new Color(0, 0, 0);
-    public static Color Red = new Color(255, 0, 0);
-    public static Color Orange = new Color(255, 165, 0);
-    public static Color Yellow = new Color(255, 255, 0);
-    public static Color Green = new Color(0, 128, 0);
-    public static Color Blue = new Color(0, 0, 255);
-    public static Color Purple = new Color(128, 0, 128);
-
+    public static Color White  { get; } = new Color(255, 255, 255);
+    public static Color Black  { get; } = new Color(0, 0, 0);
+    public static Color Red    { get; } = new Color(255, 0, 0);
+    public static Color Orange { get; } = new Color(255, 165, 0);
+    public static Color Yellow { get; } = new Color(255, 255, 0);
+    public static Color Green  { get; } = new Color(0, 128, 0);
+    public static Color Blue   { get; } = new Color(0, 0, 255);
+    public static Color Purple { get; } = new Color(128, 0, 128);
 
     public Color(int R, int G, int B)
     {
@@ -25,7 +27,4 @@ class Color
         this.G = G;
         this.B = B;
     }
-
-
-
 }

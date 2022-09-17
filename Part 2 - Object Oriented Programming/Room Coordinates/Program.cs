7f23d9e0b494e18edc2359418ec0af7465210d8a
STEEL\Jay <jay.sanchez287@gmail.com>
2022-09-17 10:57:02 -0400

    Completed Challenge - Room Coordinates

---
+++ Part 2 - Object Oriented Programming/Room Coordinates/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Title = "Room Coordinates";
+
+Coordinate room1 = new Coordinate(1, 2);
+Coordinate room2 = new Coordinate(2, 3);
+Coordinate room3 = new Coordinate(3, 4);
+
+
+Console.WriteLine($"room1 is adjacent to room2: {room1.isAdjecent(room2)}");
+Console.WriteLine($"room2 is adjacent to room3: {room2.isAdjecent(room3)}");
+Console.WriteLine($"room3 is adjacent to room1: {room3.isAdjecent(room1)}");
+
+
+
+struct Coordinate
+{
+    int Row { get; init; }
+    int Column { get; init; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public bool isAdjecent(Coordinate room)
+    {
+        bool isAdjecent;
+
+        isAdjecent = this.Row + 1 == room.Row  || this.Row - 1 == room.Row ? true : false;
+        isAdjecent = this.Column + 1 == room.Column || this.Column - 1 == room.Column ? true : false;
+
+        return isAdjecent;
+    }
+}

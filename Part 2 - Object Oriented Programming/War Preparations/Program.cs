bd2df5075d3672fef01b4cc5d53d75e8cd639f2b
STEEL\Jay <jay.sanchez287@gmail.com>
2022-09-18 03:46:11 -0400

    Completed Challenge - War Preparations

---
+++ Part 2 - Object Oriented Programming/War Preparations/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Title = "War Preparations";
+
+Sword ironSword = new Sword(SwordMaterial.Iron, Gemstone.None, 50 , 7);
+Sword rubySteelSword = ironSword with { stone = Gemstone.Ruby, material = SwordMaterial.Steel};
+Sword UltimateSword = ironSword with { stone = Gemstone.Bitstone, material = SwordMaterial.Binarium};
+
+Console.WriteLine($"{nameof(ironSword)}: {ironSword.ToString()}\n");
+Console.WriteLine($"{nameof(rubySteelSword)}: {rubySteelSword.ToString()}\n");
+Console.WriteLine($"{nameof(UltimateSword)}: {UltimateSword.ToString()}");
+
+internal record Sword(SwordMaterial material, Gemstone stone, float length, float crossguardWidth)
+{
+    public override string ToString()
+    {
+        return $"a {length}inch {material} blade with a {crossguardWidth}inch crossguard. Stone type: {stone}";
+    }
+}
+
+internal enum SwordMaterial { Wood, Bronze, Iron, Steel, Binarium }
+internal enum Gemstone { Emerald, Amber, Sapphire, Diamond, Ruby, Bitstone, None}

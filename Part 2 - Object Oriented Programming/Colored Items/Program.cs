ab4ab30e8cbb61070d438879a33d7ab166d7448b
STEEL\Jay <jay.sanchez287@gmail.com>
2022-09-20 23:14:39 -0400

    Completed Challenge - Colored Items

---
+++ Part 2 - Object Oriented Programming/Colored Items/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Title = "Colored Items";
+
+ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+sword.Display();
+bow.Display();
+axe.Display();
+
+public class ColoredItem<Thing>
+{
+    public Thing Weapon { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(Thing weapon, ConsoleColor color)
+    { 
+        Weapon = weapon;
+        Color = color;
+    }
+
+    public void Display()
+    { 
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Weapon);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }

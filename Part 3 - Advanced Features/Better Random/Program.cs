80072ca313f996db6ce23f8675da7e666b5363ff
STEEL\Jay <jay.sanchez287@gmail.com>
2022-11-27 04:00:50 -0500

    Completed Challenge - Better Random

---
+++ Part 3 - Advanced Features/Better Random/Program.cs
@@ -0,0 +1,9 @@
+﻿using RandomExtensions;
+
+Console.Title = "Better Random";
+
+Random random = new Random();
+
+Console.WriteLine(random.NextDouble(10));
+Console.WriteLine(random.NextString("Up", "Down", "Left", "Right"));
+Console.WriteLine(random.CoinFlip(9999));
---
+++ Part 3 - Advanced Features/Better Random/RandomExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace RandomExtensions;
+
+public static class RandomExtensions
+{
+    public static double NextDouble(this Random random, int maxNumber)
+    {
+        return random.NextDouble() * maxNumber;
+    }
+
+    public static string NextString(this Random random, params string[] words)
+    {
+        int pickedNumber = random.Next(words.Length);
+        return words[pickedNumber];
+    }
+
+    public static string CoinFlip(this Random random, int headsOdds = 50)
+    {
+        int roll = random.Next(101);
+
+        if (roll < Math.Clamp(headsOdds, 0, 100))
+        {
+            return "Heads!";
+        }
+        else if (roll == 50)
+        {
+            return "The Coin landed landed exactly on its edge.";
+        }
+        else return "Tails!";
+    }
+}

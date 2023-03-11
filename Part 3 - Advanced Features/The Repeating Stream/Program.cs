6b08d0c152c5059bba0efe334eefde3212d0140b
STEEL\Jay <jay.sanchez287@gmail.com>
2023-03-11 07:51:07 -0500

    Completed Challenge - The Repeating Stream

---
+++ Part 3 - Advanced Features/The Repeating Stream/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Title = "The Repeating Stream";
+
+Random random = new Random();
+RecentNumbers recentNumbers = new RecentNumbers();
+Thread numberThread = new Thread(() => NumberGenerator(random, recentNumbers));
+numberThread.Start();
+
+while (true)
+{
+    if (Console.ReadKey(true) != null)
+    {
+        lock (recentNumbers)
+        {
+            if (recentNumbers.LeastRecent == recentNumbers.MostRecent)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Bingo! That's a repeat!\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nope, those don't match\n");
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
+void NumberGenerator(Random random, RecentNumbers recentNumbers)
+{
+    while (true)
+    {
+        int newNumber = random.Next(10);
+        Console.WriteLine($"Your new number is: {newNumber}\n");
+        lock (recentNumbers)
+        {
+            recentNumbers.LeastRecent = recentNumbers.MostRecent;
+            recentNumbers.MostRecent = newNumber;
+        }
+        Thread.Sleep(1000);
+    }
+}
+
+class RecentNumbers
+{
+    public int LeastRecent { get; set; }
+    public int MostRecent { get; set; }
+}

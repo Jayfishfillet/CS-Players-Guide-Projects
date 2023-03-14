3a18957032ac987b4b71975b602116f92e480935
STEEL\Jay <jay.sanchez287@gmail.com>
2023-03-14 06:05:18 -0400

    Completed Uniter of Words and The Robot Factory

---
+++ Part 3 - Advanced Features/The Robot Factory/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Dynamic;
+
+Console.Title = "The Robot Factory";
+
+dynamic robot;
+int robotID = 1;
+
+while (true)
+{
+    robot = new ExpandoObject();
+
+    robot.ID = robotID;
+    Console.WriteLine($"You are making Robot ID: {robot.ID}\n");
+    Console.Write("Would you like to name the Robot? ");
+    string? input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Please enter a name: ");
+        robot.Name = Console.ReadLine().ToLower();
+    }
+    else
+    {
+        Console.WriteLine("Moving on...\n");
+        Thread.Sleep(1500);
+    }
+
+    Console.Write("Would you like to customize the size? ");
+    input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Please enter a height: ");
+        robot.Height = Console.ReadLine().ToLower();
+        Console.Write("Please enter a width: ");
+        robot.Width = Console.ReadLine().ToLower();
+    }
+    else
+    {
+        Console.WriteLine("Moving on...\n");
+        Thread.Sleep(1500);
+    }
+
+    Console.Write("Would you like to have colored markings? ");
+    input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Please enter a color for the markings: ");
+        robot.Color = Console.ReadLine().ToLower();
+    }
+    else
+    {
+        Console.WriteLine("Completing build...\n");
+        Thread.Sleep(1500);
+    }
+
+    Console.WriteLine("\n====================COMPLETED ROBOT=======================");
+    foreach (KeyValuePair<string, object> property in (IDictionary<string, object>)robot)
+    {
+        Console.WriteLine($"{property.Key}: {property.Value}");
+    }
+    Console.WriteLine("==========================================================\n");
+
+    robotID ++;
+}
---
+++ Part 3 - Advanced Features/Uniter of Adds/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Title = "Uniter of Adds";
+
+Console.WriteLine(DynamicAdd(1, 2));
+Console.WriteLine(DynamicAdd(1.1, 2.2));
+Console.WriteLine(DynamicAdd("Hello ", "World"));
+Console.WriteLine(DynamicAdd(DateTime.Now, TimeSpan.FromHours(3)));
+
+dynamic DynamicAdd(dynamic a, dynamic b) => a + b;

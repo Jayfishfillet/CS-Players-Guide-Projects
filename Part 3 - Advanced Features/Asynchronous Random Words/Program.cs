be160467acedb7bbb9d27f2d29ea48de3540c43f
STEEL\Jay <jay.sanchez287@gmail.com>
2023-03-14 01:24:53 -0400

    Completed Async challenges

---
+++ Part 3 - Advanced Features/Asynchronous Random Words/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Title = "Asynchronous Random Words";
+
+Console.Write("Enter a word: ");
+string? word = Console.ReadLine().ToLower();
+
+DateTime startTime = DateTime.Now;
+
+Task<long> randomlyRecreateTask = Task.Run(() => RandomlyRecreate(word));
+long attempts = await randomlyRecreateTask;
+
+DateTime endTime = DateTime.Now;
+TimeSpan totalTime = endTime - startTime;
+
+Console.WriteLine($"\nyou generated {word} in {totalTime.Seconds}s {totalTime.Milliseconds}ms, and took {attempts} attempts");
+
+long RandomlyRecreate(string word)
+{
+    Random random = new Random();
+    string generatedWord = "";
+    int attempts = 0;
+
+    while (true)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            attempts++;
+            generatedWord += (char)('a' + random.Next(26));
+        }
+
+        if (generatedWord == word) return attempts;
+        else generatedWord = ""; continue;
+    }
+}
---
+++ Part 3 - Advanced Features/Many Random Words/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Title = "Many Random Words";
+
+while (true)
+{
+    Console.Write("Enter a word: ");
+    string word = Console.ReadLine().ToLower();
+    await Task.Run(() => TaskRunner(word));
+}
+
+async Task TaskRunner(string word)
+{
+    Task<(long, TimeSpan)> randomlyRecreateTask = Task.Run(() => RandomlyRecreate(word));
+    (long attempts, TimeSpan totalTime) result = await randomlyRecreateTask;
+
+    Console.WriteLine($"You generated \"{word}\" in {result.totalTime.Seconds}s {result.totalTime.Milliseconds}ms\nAttempts: {result.attempts}\n");
+}
+
+(long, TimeSpan) RandomlyRecreate(string word)
+{
+    Random random = new Random();
+    string generatedWord = "";
+    int attempts = 0;
+
+    DateTime startTime = DateTime.Now;
+    while (true)
+    {
+        attempts++;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            generatedWord += (char)('a' + random.Next(26));
+        }
+
+        if (generatedWord == word) 
+        {
+            DateTime endTime = DateTime.Now;
+            TimeSpan totalTime = endTime - startTime;
+            return (attempts, totalTime);
+        }
+        else generatedWord = ""; continue;
+    }
+}

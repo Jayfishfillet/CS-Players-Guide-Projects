5e819bcf93b8fb376f5f42fcff37d132f9c98e05
STEEL\Jay <jay.sanchez287@gmail.com>
2023-03-17 01:09:42 -0400

    Completed Challenge - Colored Console

---
+++ Part 3 - Advanced Features/Colored Console/Colored Console/Program.cs
@@ -0,0 +1,6 @@
+﻿using ColoredConsoleLib;
+
+Console.Title = "Colored Console";
+
+string name = ColoredConsole.Prompt("What is your name? ");
+ColoredConsole.WriteLine($"Hi {name}", ConsoleColor.Magenta);
---
+++ Part 3 - Advanced Features/Colored Console/ColoredConsoleLib/ColoredConsole.cs
@@ -0,0 +1,27 @@
+﻿namespace ColoredConsoleLib;
+
+public static class ColoredConsole
+{
+    public static string Prompt(string question)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write(question);
+        string? response = Console.ReadLine();
+        Console.ResetColor();
+        return response;
+    }
+
+    public static void WriteLine(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+
+    public static void Write(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+        Console.ResetColor();
+    }
+}
